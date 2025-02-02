8aee8972e246672de42dee8d6a5d098dc5f6687f
HelderMucondo33 <heldermucondo@gmail.com>
2025-02-02 14:30:49 +0200

    pegando dados da api

--- react-previsao/src/App.jsx
+++ react-previsao/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState, useRef } from "react";
+import axios from "axios";
+import WeatherInfo from "./components/WeatherInfo/WeatherInfo";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [weather, setWeather] = useState({});
+
   const inputCidade = useRef()
 
-  function searchCity() {
+  async function searchCity() {
 
-    console.log(inputCidade.current.value);
+    const city = inputCidade.current.value;
+    const key = "5dedf7180bfd230cb02777954019a575"
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt&units=metric`
+
+    const apiInfo = await axios.get(url)
+
+    setWeather(apiInfo.data)
+
+
   }
 
   return (
@@ -16,6 +28,8 @@ function App() {
       <h1>HELDER Previsao de Tempo</h1>
       <input type="text" placeholder="digite a cidade" ref={inputCidade}/>
       <button onClick={searchCity}>Pesquisar</button>
+
+      <WeatherInfo weather = {weather}/>
     </div>
   );
 }
---
+++ react-previsao/src/components/WeatherInfo/WeatherInfo.jsx
@@ -0,0 +1,15 @@
+
+
+function WeatherInfo({weather}){
+
+
+    console.log(weather)
+return(
+    <div>
+        <h1>Meu component</h1>
+    </div>
+)
+
+}
+
+export default WeatherInfo
