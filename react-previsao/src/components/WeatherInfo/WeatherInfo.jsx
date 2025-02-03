43bf853f88a98a6b5f6de2d8d5ebc4ffeeaca61a
HelderMucondo33 <heldermucondo@gmail.com>
2025-02-03 23:07:32 +0200

    adicionando estilo informacoes do estado do tempo

--- react-previsao/src/components/WeatherInfo/WeatherInfo.jsx
+++ react-previsao/src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,20 +1,20 @@
-
+import './WeatherInfo.css'
 
 function WeatherInfo({ weather }) {
   
 
   return (
-    <div>
+    <div className='weather-container'>
       <h2>{weather.name}</h2>
-      <div>
+      <div className='weather-info'>
         <img
           src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
           alt="Ícone do clima"
         />
-        <p>{Math.round(weather.main.temp)}℃</p>
+        <p className='temperature'>{Math.round(weather.main.temp)}℃</p>
       </div>
-      <p>{weather.weather[0].description}</p>
-      <div>
+      <p className='description'>{weather.weather[0].description}</p>
+      <div className='detail'>
         <p>Sensacao termica: {Math.round(weather.main.feels_like)}</p>
         <p>Unidade: {weather.main.humidity}</p>
         <p>Pressao: {weather.main.pressure}</p>
